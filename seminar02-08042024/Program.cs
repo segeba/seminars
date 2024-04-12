c36ff192e9f02b640780fac0cd80a43014b6f87e
Sergey Bartasevich <sbartasevich@gmail.com>
2024-04-12 11:00:59 +0300

    add method NumbSquare()

--- seminar02-08042024/Program.cs
+++ seminar02-08042024/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void NumbSquare(int numb) //Простой метод "void", без возврата информации
+{
+    System.Console.WriteLine(numb * numb);
+}
+
+System.Console.WriteLine("Input number: "); //вывод строки "Input number: " (WriteLine() - построчный вывод)
+int a = Convert.ToInt32(Console.ReadLine()); //присваивание переменной a введенного значения (Console.ReadLine())
+
+System.Console.Write($"The square of the number {a} is: "); //(Write() - вывод в одну строку)
+NumbSquare(a); //вызов метода NumbSquare()
