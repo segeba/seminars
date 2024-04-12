90626fa3b77671f3707565afa457615eb28a63b5
Sergey Bartasevich <sbartasevich@gmail.com>
2024-04-12 11:12:45 +0300

    add method NumbSquareInt()

--- seminar02-08042024/Program.cs
+++ seminar02-08042024/Program.cs
@@ -1,10 +1,16 @@
-﻿void NumbSquare(int numb) //Простой метод "void", без возврата информации
+﻿void NumbSquare(int numb) //простой метод "void", без возврата значения
 {
-    System.Console.WriteLine(numb * numb);
+    System.Console.WriteLine($"1. The square of the number {numb} is: {numb * numb}"); //(Write() - вывод в одну строку)
 }
 
+int NumbSquareInt(int numb) //метод с возвратом значения
+{
+    return(numb * numb); //обязательный оператор в методе с возвратом значения
+}
 System.Console.WriteLine("Input number: "); //вывод строки "Input number: " (WriteLine() - построчный вывод)
 int a = Convert.ToInt32(Console.ReadLine()); //присваивание переменной a введенного значения (Console.ReadLine())
 
-System.Console.Write($"The square of the number {a} is: "); //(Write() - вывод в одну строку)
 NumbSquare(a); //вызов метода NumbSquare()
+
+int result = NumbSquareInt(a);
+System.Console.WriteLine($"2. The square of the number {a} is: {result}");
