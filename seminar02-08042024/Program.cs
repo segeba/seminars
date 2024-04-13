45d0b09e57bf8e4ed55671f4fb5e916bec3c9ff1
Sergey Bartasevich <sbartasevich@gmail.com>
2024-04-13 23:54:23 +0300

    add program 3

--- seminar02-08042024/Program.cs
+++ seminar02-08042024/Program.cs
@@ -16,11 +16,11 @@
 // System.Console.WriteLine($"2. The square of the number {a} is: {result}");
 // System.Console.WriteLine($"3. The square of the number {a} is: {NumbSquareInt(a)}");
 
-// Программа 1
-// принимает на вход трехзначное число и удаляет вторую цифру этого числа
-// например:
-// 256 => 26
-// 891 => 81
+// // Программа 1
+// // принимает на вход трехзначное число и удаляет вторую цифру этого числа
+// // например:
+// // 256 => 26
+// // 891 => 81
 
 // int DeleteSecondDigit(int num)
 // {
@@ -42,34 +42,61 @@
 
 // System.Console.WriteLine(DeleteSecondDigit(num));
 
-// Програма 2
-// принимает на вход трехзначное число и возводит вторую цифру в степень, равную третьей цифре
-// например:
-// 487 => 8^7 = 2 097 152
-// 254 => 5^4 = 625
-// 617 => 1^7 = 1
+// // Програма 2
+// // принимает на вход трехзначное число и возводит вторую цифру в степень, равную третьей цифре
+// // например:
+// // 487 => 8^7 = 2 097 152
+// // 254 => 5^4 = 625
+// // 617 => 1^7 = 1
 
-int Exponentiation(int num)
+// int Exponentiation(int num)
+// {
+//     if (num >= 100 && num <= 999) // проверка числа на трехзначность
+//     {
+//         int ed = num % 10;
+//         int dec = num / 10 % 10;
+//         int result = 1;
+//         for (int i = 0; i < ed; i++) // цикл возведения второй цифры в степень третьей цифры
+//         {
+//             result = result * dec;
+//         }
+//         return result;
+//     }
+//     else
+//     {
+//         System.Console.WriteLine("Your input not three-digit number!");
+//         return 0;
+//     }
+// }
+
+// Console.Write("Input three-digit number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// System.Console.WriteLine(Exponentiation(num));
+
+// Програма 3
+// принимает на вход два числа и проверяет, кратно ли первое число второму.
+// если первое число не кратно второму, то программа выводит остаток от деления первого числа на второе
+// например:
+// 14, 5 -> no, 4
+// 16, 8 -> yes
+// 4, 3 -> no, 1
+
+void MultiplicityCheck(int num1, int num2)
 {
-    if (num >= 100 && num <= 999) // проверка числа на трехзначность
+    if (num1 % num2 == 0) // проверка деления числа num1  на число num2 без остатка
     {
-        int ed = num % 10;
-        int dec = num / 10 % 10;
-        int result = 1;
-        for (int i = 0; i < ed; i++) // цикл возведения второй цифры в степень третьей цифры
-        {
-            result = result * dec;
-        }
-        return result;
+        System.Console.WriteLine($"{num1}, {num2} -> yes");
     }
     else
     {
-        System.Console.WriteLine("Your input not three-digit number!");
-        return 0;
+        System.Console.WriteLine($"{num1}, {num2} -> no, {num1 % num2}");
     }
 }
 
-Console.Write("Input three-digit number: ");
-int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input Number 1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input Number 2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
 
-System.Console.WriteLine(Exponentiation(num));
+MultiplicityCheck(num1, num2);
