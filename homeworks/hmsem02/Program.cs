be0ebe357f214beba77cbb1b92fb3102ddcddd56
Sergey Bartasevich <sbartasevich@gmail.com>
2024-04-14 00:16:20 +0300

    add task 1 in homeworks/hmsem02/Programs.cs

--- homeworks/hmsem02/Program.cs
+++ homeworks/hmsem02/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1
+// программа принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+void MultiplicityCheck(int num)
+{
+    if (num % 7 == 0 && num % 23 == 0)
+    {
+        System.Console.WriteLine($"Число {num} кратно одновременно 7 и 23!");
+    }
+    else
+    {
+         System.Console.WriteLine($"Число {num} НЕ кратно одновременно 7 и 23!");
+    }
+}
+
+Console.Write("Input three-digit number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+MultiplicityCheck(num);
