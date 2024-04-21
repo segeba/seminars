5e8d6d28227859efaf080bcb6754179599b816f0
Sergey Bartasevich <sbartasevich@gmail.com>
2024-04-21 22:29:09 +0300

    add .\seminar05-18042024"

---
+++ seminar05-18042024/Program.cs
@@ -0,0 +1,116 @@
+﻿int[,] CreateMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = new Random().Next(min, max + 1);//[min, max] [0, 9]
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+    System.Console.WriteLine();
+}
+
+int[,] EvenIndxSquare(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if(i % 2 == 0 && j % 2 == 0)
+            {
+                matrix[i, j] = matrix[i, j] * matrix[i, j];
+            }
+        }
+    }
+    return matrix;
+}
+
+//Задайте двумерный массив. Найдите сумму элементов,
+// находящихся на главной диагонали (с индексами (0,0); (1;1) и
+// т.д.
+// Пример
+// 2 3 4 3
+//  4 3 4 1 => 2 + 3 + 5 = 10
+//  2 9 5 4
+
+int SumMatrix(int[,] matrix)
+{
+    int sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if(i == j )
+            {
+                sum += matrix[i, j]; //sum = sum + matrix [i, j]
+            }
+        }
+    }
+    return sum;
+}
+
+// Задайте двумерный массив из целых чисел. Сформируйте новый
+// одномерный массив, состоящий из средних арифметических
+// значений по строкам двумерного массива.
+// Пример
+// 2 3 4 3
+// 4 3 4 1
+double[] MidlSum(int[,] matrix)
+{
+
+    double[] arrow = new double[matrix.GetLength(0)];
+    double sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum += matrix[i, j];
+        }
+        arrow[i] = (double) sum / matrix.GetLength(1);
+        sum = 0;
+    }
+    return arrow;
+}
+
+void PrintArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+    }
+    System.Console.WriteLine();
+}
+
+System.Console.WriteLine("Input number of rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input number of columns: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input minimal value of arr element: ");
+int min = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input maximal value of arr element: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = CreateMatrix(rows, columns, min, max);
+System.Console.WriteLine("Автоматически сгенерированный массив:");
+PrintMatrix(matrix);
+System.Console.WriteLine($"Сумма элементов массива по диогонали - {SumMatrix(matrix)}");
+System.Console.WriteLine();
+System.Console.WriteLine("Одномерный массив, состоящий из средних арифметических значений по строкам двумерного массива:");
+PrintArray(MidlSum(matrix));
+System.Console.WriteLine();
+System.Console.WriteLine("Перезаписанный массив с возведенными в квадрат элементами, расположенными в четных строках и столбцах одновременно:");
+PrintMatrix(EvenIndxSquare(matrix));
