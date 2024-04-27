d87006a668e5aa40078045acbe7d99e36ad1641b
Sergey Bartasevich <sbartasevich@gmail.com>
2024-04-27 21:36:23 +0300

    edit01 .\hmsem07

--- homeworks/hmsem07/Program.cs
+++ homeworks/hmsem07/Program.cs
@@ -16,10 +16,18 @@ void FromMtoN(int m, int n)
     }
     else System.Console.Write(m + " ");
 }
+
+System.Console.WriteLine($"Natural numbers from {m} to {n}:");
+FromMtoN(m, n);
+
+// Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа M и N.
+
+
+
+
+
 System.Console.WriteLine("Input number M: ");
 int m = Convert.ToInt32(Console.ReadLine());
 System.Console.WriteLine("Input number N: ");
 int n = Convert.ToInt32(Console.ReadLine());
-
-System.Console.WriteLine($"Natural numbers from {m} to {n}:");
-FromMtoN(m, n);
